f4d2fa3909ee8e285c19ce93e26fe431c5b600db
ibrokhim.sidikov <ibrokhim.sidikov@gmail.com>
2024-05-04 12:03:35 +0500

    Task-01-08--part2

--- lib/pages/combopage.dart
+++ lib/pages/combopage.dart
@@ -1,5 +1,5 @@
+import 'package:apploook/widget/widget_support.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ComboPage extends StatefulWidget {
   const ComboPage({super.key});
@@ -11,6 +11,146 @@ class ComboPage extends StatefulWidget {
 class _ComboPageState extends State<ComboPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          //fooditem1
+          margin: EdgeInsets.only(right: 5.0),
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/IMG_3257.png',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 20.0),
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Dinner Meal Normal",
+                          style: AppWidget.semiboldTextFieldStyle(),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Chicken 3pcs, Coca-cCola 0.5L, Coleslaw",
+                          style: AppWidget.LightTextFieldStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          //fooditem1
+          margin: EdgeInsets.only(right: 5.0),
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/IMG_3257.png',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 20.0),
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Dinner Meal Normal",
+                          style: AppWidget.semiboldTextFieldStyle(),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Chicken 3pcs, Coca-cCola 0.5L, Coleslaw",
+                          style: AppWidget.LightTextFieldStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          //fooditem1
+          margin: EdgeInsets.only(right: 5.0),
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/IMG_3257.png',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 20.0),
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Dinner Meal Normal",
+                          style: AppWidget.semiboldTextFieldStyle(),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          "Chicken 3pcs, Coca-cCola 0.5L, Coleslaw",
+                          style: AppWidget.LightTextFieldStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
--- lib/pages/homenew.dart
+++ lib/pages/homenew.dart
@@ -37,125 +37,123 @@ class _HomeNewState extends State<HomeNew> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-          margin: const EdgeInsets.only(top: 10.0),
-          child: Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.5,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 226, 225, 225),
+        margin: const EdgeInsets.only(top: 10.0),
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 226, 225, 225),
+              ),
+            ),
+            // Body Container
+            Container(
+              margin:
+                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            ),
+            Positioned(
+              top: 40,
+              left: 15,
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 40,
+                  color: Colors.black,
                 ),
               ),
-              // Body Container
-              Container(
-                margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height / 3),
-                height: MediaQuery.of(context).size.height / 2,
-                width: MediaQuery.of(context).size.width,
+            ),
+            Positioned(
+              top: 40,
+              right: 15,
+              child: Container(
+                padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min, // Avoid unnecessary space
+                  children: [
+                    Icon(Icons.location_on, size: 30),
+                    SizedBox(width: 5.0), // Add some horizontal spacing
+                    Text("Istanbul"),
+                  ],
+                ),
               ),
-              Positioned(
-                top: 40,
-                left: 15,
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 40,
-                    color: Colors.black,
+            ),
+            Positioned(
+              top: 105,
+              left: 15,
+              child: Container(
+                child: Text(
+                  'WHAT`S NEW',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 140,
+              left: 15,
+              child: Container(
+                child: Row(
+                  children: [
+                    Image.asset('images/sale50offburgers.png'),
+                    SizedBox(width: 10.0),
+                    Image.asset('images/sale50offburgers.png')
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 300.0,
+              left: 0.0,
+              right: 10.0,
+              child: DefaultTabController(
+                length: tabTitles.length,
+                child: Material(
+                  color: Colors.transparent,
+                  child: TabBar(
+                    isScrollable: true, // Enable horizontal scrolling
+                    labelPadding: const EdgeInsets.symmetric(
+                        horizontal: 10.0), // Adjust spacing
+                    tabs: tabTitles.map((title) => Tab(text: title)).toList(),
+                    onTap: (index) => setState(() => selectedTabIndex = index),
                   ),
                 ),
               ),
-              Positioned(
-                top: 40,
-                right: 15,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min, // Avoid unnecessary space
-                    children: [
-                      Icon(Icons.location_on, size: 30),
-                      SizedBox(width: 5.0), // Add some horizontal spacing
-                      Text("Istanbul"),
-                    ],
-                  ),
+            ),
+            Positioned(
+              top: (MediaQuery.of(context).size.height / 2.5) +
+                  10, // Adjust offset
+              left: 10.0,
+              right: 10.0,
+              bottom: 0.0,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
                 ),
-              ),
-              Positioned(
-                top: 105,
-                left: 15,
-                child: Container(
-                  child: Text(
-                    'WHAT`S NEW',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 140,
-                left: 15,
-                child: Container(
-                  child: Row(
-                    children: [
-                      Image.asset('images/sale50offburgers.png'),
-                      SizedBox(width: 10.0),
-                      Image.asset('images/sale50offburgers.png')
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 300.0,
-                left: 0.0,
-                right: 10.0,
-                child: DefaultTabController(
-                  length: tabTitles.length,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: TabBar(
-                      isScrollable: true, // Enable horizontal scrolling
-                      labelPadding: const EdgeInsets.symmetric(
-                          horizontal: 10.0), // Adjust spacing
-                      tabs: tabTitles.map((title) => Tab(text: title)).toList(),
-                      onTap: (index) =>
-                          setState(() => selectedTabIndex = index),
+                child: Column(
+                  children: [
+                    IndexedStack(
+                      index: selectedTabIndex,
+                      children: contentPages.values.toList(),
                     ),
-                  ),
+                  ],
                 ),
               ),
-              Positioned(
-                top: (MediaQuery.of(context).size.height / 2.5) +
-                    10, // Adjust offset
-                left: 10.0,
-                right: 10.0,
-                bottom: 0.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      IndexedStack(
-                        index: selectedTabIndex,
-                        children: contentPages.values.toList(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
