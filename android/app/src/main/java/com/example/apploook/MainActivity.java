1717189eb868259b16ed6eeb8f915516e7b0913e
JamoliddinDev <mrjamaliddin1555@gmail.com>
2024-05-23 16:17:20 +0500

    android notification completed

--- android/app/src/main/java/com/example/apploook/MainActivity.java
+++ android/app/src/main/java/com/example/apploook/MainActivity.java
@@ -1,6 +1,29 @@
 package com.example.apploook;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Channel ID needs to match the ID you use in your app
+            String channelId = "default_channel_id";
+            String channelName = "Default Channel";
+            String channelDescription = "This is the default notification channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+            channel.setDescription(channelDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:apploook/api/firabase_api.dart';
 import 'package:apploook/cart_provider.dart';
 import 'package:apploook/pages/checkout.dart';
 import 'package:apploook/pages/homenew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseApi().initNotifications();
   runApp(
     ChangeNotifierProvider(
       create: (context) => CartProvider(),
